2d2f43b36a7eaad78802b269255ae258fd04292f
Kei18 <keisuke.oku18@gmail.com>
2020-11-26 20:21:22 +0900

    modify bugs

--- unlabeled_mapf/src/goal_allocator.cpp
+++ unlabeled_mapf/src/goal_allocator.cpp
@@ -24,15 +24,14 @@ void GoalAllocator::assign()
     if (a.start_index != b.start_index) return a.start_index < b.start_index;
     return a.g->id < b.g->id;
   };
+
+  // setup open list
   std::priority_queue<LibGA::FieldEdge,
                       std::vector<LibGA::FieldEdge>,
                       decltype(compare)> OPEN(compare);
 
-  auto G = P->getG();
-  auto goals = P->getConfigGoal();
   for (int i = 0; i < P->getNum(); ++i) {
     auto s = P->getStart(i);
-    G->BFS(s, goals);
     for (int j = 0; j < P->getNum(); ++j) {
       auto g = P->getGoal(j);
       OPEN.emplace(i, j, s, g, s->manhattanDist(g));
