dc0c191a42d160326fb0d20d4b1b68306f0b5ee4
Kei18 <keisuke.oku18@gmail.com>
2020-11-30 16:34:32 +0900

    modify exp script

--- exp_scripts/run.sh
+++ exp_scripts/run.sh
@@ -21,27 +21,36 @@ then
 fi
 GIT_RECENT_COMMIT=`git log -1 --pretty=format:"%H"`
 
+## print experimental info
+echo "-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+echo "start experiment"
+echo "- map: ${map}"
+echo "- agents: ${agents_list}"
+echo "- solver: ${solver}"
+echo "- flocking_blocks: ${flocking_blocks}"
+echo "- scen: from ${scen_start} to ${scen_end}"
+
 ## create output directory
-EXP_DATE=`date +%Y-%m-%d-%H-%M-%S`
+EXP_DATE=`gdate +%Y-%m-%d_%H-%M-%S-%2N`
 OUTPUT_DIR=$PROJECT_DIR/../data/$EXP_DATE/
 mkdir -p $OUTPUT_DIR
 
 ## build
-(cd $PROJECT_DIR/build; make;)
+(cd $PROJECT_DIR/build;
+ make > /dev/null 2>&1;)
 
 ## main
 for agent_num in ${agents_list[@]}
 do
+    echo "\nagents=${agent_num}"
     scen=$scen_start
-    while [ $scen -lt $scen_end ]
+    while [ $scen -lt `expr $scen_end + 1` ]
     do
         scen_file="${map_trimed}_${agent_num}agents_${flocking_blocks}blocks_${scen}.txt"
         if [ $flocking_blocks -eq 0 ]
         then
             scen_file="${map_trimed}_${agent_num}agents_${scen}.txt"
         fi
-        echo $scen_file
-
         $PROJECT_DIR/build/app \
                     -i $PROJECT_DIR/instances/$scen_file \
                     -o $OUTPUT_DIR/$scen_file \
@@ -63,3 +72,7 @@ STATUS_FILE=$OUTPUT_DIR/status.txt
     echo scen_end:$scen_end
     echo flocking_blocks:$flocking_blocks
 } > $STATUS_FILE
+
+echo "\nfinish experiment"
+echo "result -> ${OUTPUT_DIR}"
+echo "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"
